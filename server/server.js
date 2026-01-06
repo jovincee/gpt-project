443f534b74b3af7cb60efc15a34fb889ed539627
John Penaflor <johnvin.penaflor@gmail.com>
2026-01-06 14:25:05 -0700

    Server.js edited

--- server/server.js
+++ server/server.js
@@ -16,7 +16,15 @@ await connectDB()
 
 
 //middleware
-app.use(cors())
+// Middleware
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://gpt-project-three.vercel.app/"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true
+}));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
