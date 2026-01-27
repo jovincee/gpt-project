384272e211790bd7db241a638fc8ce59851baa5e
John Penaflor <johnvin.penaflor@gmail.com>
2026-01-26 18:20:46 -0700

    Commit new and edited api.js file

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_SERVER_URL,
-  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
