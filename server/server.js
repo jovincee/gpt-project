91f29a8584464cb551366ec080bba7b1aeb646b7
John Penaflor <johnvin.penaflor@gmail.com>
2026-01-26 21:10:05 -0700

    Commit to server.js

--- server/server.js
+++ server/server.js
@@ -19,6 +19,10 @@ const app = express()
 await connectDB()
 
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+
 //middleware
 // Middleware
 app.use(cors({
@@ -32,8 +36,7 @@ app.use(cors({
 }));
 
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
+
 
 
 
